fff521ced8b95301b83fba20d856f60396ee9ee8
LeoQuint <leonardquintal@gmail.com>
2016-02-21 13:43:47 -0500

    Got a decent first draft of the world selection menu. No 100% sure on what would be nicer for level selection inside thoses worlds.

---
+++ Assets/_FrameWork/Controllers/CTR_LevelSelection.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public enum GameWorld 
+{
+    JUNKYARD,
+    BALLOON,
+    TEMP1,
+    TEMP2,
+    TEMP3,
+    TEMP4,
+    TEMP5,
+    COUNT
+}
+
+public class CTR_LevelSelection : MonoBehaviour {
+
+    [SerializeField][Tooltip("Set to Starting world.")]
+    GameWorld currentWorld;
+
+    
+
+    public void SetCurrentWorld(string world)
+    {
+        currentWorld = (GameWorld)System.Enum.Parse(typeof(GameWorld), world);
+    }
+
+    public void LoadSelectedWorld() 
+    {
+        Debug.Log("World " + currentWorld + " Selected");
+        SceneManager.LoadScene("w_2_lvl_1_Junkyard");
+    }
+
+   
+	
+}
