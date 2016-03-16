2281e30b63e20dd9c11cbe04bf412bfe35360b0a
briangogarty <brian.gogarty@gmail.com>
2016-03-16 18:29:52 -0400

    added cinematic scene

---
+++ Assets/Cinematic/YMMCCinematic.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YMMCCinematic : MonoBehaviour {
+    public MovieTexture ymmcMovie;
+    float timePassed;
+    bool startCounting;
+	// Use this for initialization
+	void Start () 
+    {
+        GetComponent<Renderer>().material.mainTexture = ymmcMovie;
+        ymmcMovie.Play();
+        startCounting = true;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        if(startCounting == true)
+        {
+            timePassed += Time.fixedDeltaTime;
+        }
+        if(timePassed >= ymmcMovie.duration)
+        {
+            Application.LoadLevel(1);
+        }
+	}
+}
