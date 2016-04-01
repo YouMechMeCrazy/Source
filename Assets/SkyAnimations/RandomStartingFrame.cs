2f0b1899643aefcf8cce80fb5490458c30754459
LeoQuint <leonardquintal@gmail.com>
2016-03-31 21:30:31 -0400

    fixed the star animation

--- Assets/SkyAnimations/RandomStartingFrame.cs
+++ Assets/SkyAnimations/RandomStartingFrame.cs
@@ -4,41 +4,16 @@ using System.Collections;
 //Starts the animator on this at a random frame
 public class RandomStartingFrame : MonoBehaviour 
 {
-    int fDelay = 25;
-    int cFrame = 0;
 
-    bool reset = false;
-
-    float originalSpeed = 1f;
-	// Use this for initialization
 	void Awake ()
     {
         //Ignore if no animator
         if (this.GetComponent<Animator>() == null)
             return;
+     
+        GetComponent<Animator>().Play("StarAnim", -1, Random.Range(0f, 1f));
 
        
-
-        //Save original speed reference and set to something insane
-        originalSpeed = this.GetComponent<Animator>().speed;
-        
-        this.GetComponent<Animator>().speed = Random.Range(0, 2000f);
-        
-       
-	}
-
-    void FixedUpdate() 
-    {
-        if (reset)
-        {
-            return;
-        }
-        if (cFrame == fDelay)
-        {
-            this.GetComponent<Animator>().speed = originalSpeed;
-        }
-        cFrame++;
-       
-    }
+	}   
 
 }
